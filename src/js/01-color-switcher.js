bf3581904daa044d503073cb86f59a65b781fecc
Dezertirr <125862316+Dezertirr@users.noreply.github.com>
2023-05-05 00:46:41 +0300

    upd

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -6,7 +6,7 @@ const colorSwitchStart = document.querySelector('button[data-start]')
 const colorSwitchStop = document.querySelector('button[data-stop]')
 const body = document.body
 
-const interval
+let interval
 
 colorSwitchStart.addEventListener('click', ()  => {
    interval = setInterval (()=> {
